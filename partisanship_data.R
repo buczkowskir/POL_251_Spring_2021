04c02a947eb9ff1e33bb05f30d4662cf793e8c99
buczkowskir <rjbuczko@go.olemiss.edu>
2021-03-08 15:22:09 -0600

    Initial Commit

---
+++ partisanship_data.R
@@ -0,0 +1,61 @@
+
+# Data Analysis of Partisanship -- 2016 Election #
+
+# Ryan Buczkowski #
+
+#------------------------------------------#
+
+# Loading Libraries
+pacman::p_load('tidyverse', 'rvest', 'ggrepel')
+
+# Creating URL object for partisanship table
+partisanship_url <- 'https://news.gallup.com/poll/203117/gop-maintains-edge-state-party-affiliation-2016.aspx'
+
+# Scraping partisanship table
+read_html(partisanship_url) %>% 
+  html_node(xpath = '//*[@id="20170126154846"]/table') %>% 
+  html_table(fill = TRUE) %>% 
+  as_tibble() -> web_table
+
+# Cleaning web table
+web_table %>% 
+  select(X1, X4) %>% 
+  slice(-c(1,2,53)) %>% 
+  mutate(X4 = parse_number(X4)) %>% 
+  rename('state_name' = X1,
+         'dem_adv'    = X4) -> partisanship_table
+
+# Creating CSV file
+partisanship_table %>% 
+  write_csv(path = 'partisanship_table.csv')
+
+# Joining data together
+read_csv('home_values.csv') %>% 
+  inner_join(partisanship_table) -> partisanship_home_values
+
+# Visualizing data
+partisanship_home_values %>% 
+  ggplot(aes(x = reorder(state_abb, dem_adv), y = dem_adv)) +
+  geom_col(aes(fill = dem_adv)) +
+  scale_fill_gradient2(low      = 'red',
+                       mid      = 'white',
+                       high     = 'blue',
+                       midpoint = 0,
+                       name = 'Democrat Advantage') +
+  theme_minimal() +
+  labs(x = 'State',
+       y = 'Democratic Advantage',
+       title = 'Democratic Advantage by State') +
+  theme(axis.text  = element_text(face = 'bold.italic'),
+        axis.title = element_text(face = 'bold',
+                                  size = 14),
+        plot.title = element_text(size = 18,
+                                  face = 'bold'))
+
+
+mtcars %>% 
+  ggplot(aes(x = wt, y = mpg)) +
+  geom_point() +
+  geom_smooth(method = 'lm') +
+  theme_minimal()
+
---
+++ women_data.R
@@ -0,0 +1,60 @@
+
+
+# Operationalizing Gender Norms Variable
+
+# Ryan Buczkowski
+
+#-----------------------------------------#
+
+# Loading Libraries
+pacman::p_load('tidyverse', 'rvest')
+
+# Creating URL object
+women_url <- 'https://statusofwomendata.org/explore-the-data/political-participation/additional-state-data/political-participation-composite/'
+
+# Scraping web table
+read_html(women_url) %>% 
+  html_node(xpath = '//*[@id="post-2937"]/table') %>% 
+  html_table(fill = TRUE) %>% 
+  as_tibble() -> women_web_table
+
+# Cleaning web table
+women_web_table %>% 
+  select(X1, X2) %>% 
+  rename('state_name'  = X1,
+         'women_index' = X2) -> women_table
+
+# Join all data together
+read_csv('home_values.csv') %>% 
+  inner_join(read_csv('partisanship_table.csv')) %>% 
+  inner_join(women_table) -> joined_unclean
+
+# Cleaning joined data
+joined_unclean %>% 
+  mutate(women_index = str_replace(women_index, '–', '-'),
+         women_index = parse_number(women_index)
+         
+  ) -> joined_clean
+
+# Creating CSV file
+joined_clean %>% 
+  write_csv(path = 'women_partisanship_home_values.csv')
+
+# Visualizing women data
+joined_clean %>% 
+  ggplot(aes(x = state_region, y = women_index)) +
+  geom_boxplot(aes(fill = state_region))
+
+joined_clean %>% 
+  arrange(women_index)
+
+# Seeing relationship between variables
+joined_clean %>% 
+  select(housing_mean, dem_adv, women_index) %>% 
+  cor()
+
+# Visualizing relationship between variables
+joined_clean %>% 
+  ggplot(aes(x = log(housing_mean), y = dem_adv)) +
+  geom_point() +
+  geom_smooth(method = 'lm')
